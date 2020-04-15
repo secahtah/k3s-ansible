94dfe4c89555a4099203b746db3b445c64a23950
Scott Dalton <secahtah@gmail.com>
2020-04-14 23:22:20 -0600

    added NFS stuff but not working

--- bootstrap/manual-shit.sh
+++ bootstrap/manual-shit.sh
@@ -6,17 +6,31 @@
 #   But you've configured networking, disabled the damned desktop,
 #   and enabled ssh.
 
+# Set hostname variable
+export NEWHOSTNAME="k3s-server"
+
 # set the hostname
-#? make variable
-hostnamectl set-hostname k3s-server
+hostnamectl set-hostname $NEWHOSTNAME
 
 # fix /etc/hosts entries after renaming the host
-#? replace raspberrypi with k3s-server
-#? make variable
-sudo sed -i 's/raspberrypi/k3s-server/g' /etc/hosts
+#   Source:
+#   https://linuxize.com/post/how-to-use-sed-to-find-and-replace-string-in-files/
+sudo sed -i 's/raspberrypi/$NEWHOSTNAME/g' /etc/hosts
 
 # set static address on it
+#? Nevermind. My router does this via DHCP. Screw it
+
+# create the directory to mount NFS
+sudo mkdir /mnt/ssd
+
+# Fix permissions BEFORE mounting it
+#? Still doesn't fucking work
+sudo chown -R pi:pi /mnt/ssd/
 
 # add NFS to /etc/fstab
+#? Also doesn't fucking work, fix me
+#  The NFS won't mount because the Pi's wifi comes up after it tries to mount
+#  the NFS
+sudo echo "10.50.0.10:/mnt/gihugic/k3s   /mnt/ssd   nfs    rw  0  0" >> /etc/fstab
 
 # 
